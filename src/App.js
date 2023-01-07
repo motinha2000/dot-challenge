17bff01e188ed86dd25e6f6077f60e0ef23ea52a
Gabriel.M <gamotasantos2000@gmail.com>
2023-01-07 12:27:08 -0400

    criação das funções de desfazer e refazer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import './App.css';
 
-
 function App() {
   const [list, setList] = useState([]);
+  const [undid, setUndid] = useState([]);
+
   const handleClick = (event) => {
     const newDot = {
       clientX: event.clientX,
@@ -12,8 +13,44 @@ function App() {
     console.log(newDot)
     setList((prev) => [...prev, newDot]);
   };
+  
+const handleUndo = (event) => {
+  event.stopPropagation();
+  console.log('undo');
+
+  if(list.lenght === 0){
+    return;
+  }
+
+  const lastItem = list[list.length -1];
+  setUndid((prev) => [...prev, lastItem]);
+
+  setList((prev) => {
+    const newArr = [...prev].slice(0,-1);;
+    return newArr;
+  });
+};
+
+const handleRedo = (event) => {
+  event.stopPropagation();
+  console.log('redo');
+
+  if(undid.length === 0){
+    return;
+  }
+
+  const recoveredDot = undid[undid.length -1];
+  setUndid((prev) => {
+    const newArr = [...prev].slice(0, -1);
+    return newArr;
+  });
+  setList((prev) => [...prev, recoveredDot]);
+};
+
   return (
-    <div id='page' onClick={(handleClick)}>
+    <div id='page' onClick={handleClick}>
+      <button class="btn" onClick={handleUndo}>Desfazer</button>
+      <button onClick={handleRedo}>Refazer</button>
       {list.map(item => (
          <span className='dot' style={{left: item.clientX, top: item.clientY}} />
          ))}
